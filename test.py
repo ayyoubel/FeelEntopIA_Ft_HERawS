53db627ae1c17bc41260e3dfdeb0efb5be5100de
ayyoubel <129528145+ayyoubel@users.noreply.github.com>
2023-08-15 23:14:08 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import streamlit as st
+import numpy as np
+import seaborn as sns
+
+df= pd.read_excel("hhh.xlsx")
+
+st.title('Scatter Plot App')
+
+# Dropdowns to select x and y columns
+x_column = st.selectbox('Select X Column', df.columns)
+y_column = st.selectbox('Select Y Column', df.columns)
+
+
+plot_button = st.button('PLOT')
+
+# Handle plot button click
+if plot_button:
+    # Create a scatter plot using Seaborn
+    plt.figure(figsize=(8, 6))
+    sns.scatterplot(data=df, x=x_column, y=y_column)
+    plt.xlabel(x_column)
+    plt.ylabel(y_column)
+    plt.title(f'Scatter Plot: {x_column} vs {y_column}')
+    st.pyplot(plt)
